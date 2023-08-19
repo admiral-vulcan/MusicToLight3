db8c7ce26755f5e3b59f42d366cee5debf57c218
admiral-vulcan <felix.rau@outlook.com>
2023-08-19 17:58:07 +0200

    cleaned comments

--- hdmi.py
+++ hdmi.py
@@ -1,19 +1,34 @@
+# MusicToLight3  Copyright (C) 2023  Felix Rau.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# Required Libraries
 import pygame
 import random
 from helpers import *
 
-# Bildschirmauflösung setzen (angepasst an deine Bildschirmauflösung)
+# Screen resolution settings
 screen_width = 1920
 screen_height = 1080
 screen = pygame.display.set_mode((screen_width, screen_height))
 
 colors = [(255, 0, 0), (255, 0, 255), (0, 0, 255)]
 font = 0
-zero_color = (8, 8, 8)
+zero_color = (16, 16, 16)
 
 
-# Pygame Initialisierung
 def init_hdmi():
+    """Initialize pygame and the display settings."""
     global font
     pygame.init()
 
@@ -22,7 +37,7 @@ def init_hdmi():
 
 
 def adjust_color(color):
-    """Justiert den Farbwert mit zufälligen Werten."""
+    """Adjust color values using random values."""
     adjusted = []
     for value in color:
         if value == 0:
@@ -35,103 +50,73 @@ def adjust_color(color):
 
 
 def hdmi_draw_black():
+    """Fill the screen with black color."""
     screen.fill((0, 0, 0))
     pygame.display.flip()
 
 
 def hdmi_draw_centered_text(text):
+    """Draw text centered on the screen."""
     global screen, font
 
-    # Textzeilen erstellen, indem man den übergebenen Text beim Newline-Zeichen teilt
     lines = text.split('\n')
-
-    # Gesamthöhe aller Zeilen berechnen
     total_height = sum([font.size(line)[1] for line in lines])
-
-    # Start-Y-Position berechnen, um den Text zentriert zu halten
     start_y = (screen.get_height() - total_height) // 2
-
-    # Bildschirm leeren (optional, falls benötigt)
-    screen.fill((0, 0, 0))  # Schwarz
-
+    screen.fill((0, 0, 0))
     for line in lines:
-        # Text rendern
-        text_surface = font.render(line, True, (255, 255, 255))  # Weißer Text
-
-        # Mittige Position in X-Richtung berechnen
+        text_surface = font.render(line, True, (255, 255, 255))
         center_x = (screen.get_width() - text_surface.get_width()) // 2
-
-        # Text auf den Bildschirm blitten
         screen.blit(text_surface, (center_x, start_y))
-
-        # Y-Position für die nächste Zeile aktualisieren
         start_y += font.size(line)[1]
-
-    # Änderungen anzeigen
     pygame.display.flip()
 
 
 @hdmi_in_thread
 def hdmi_draw_matrix(matrix):
+    """Draw a matrix of numbers with various colors and effects."""
     global screen, font, colors, zero_color
-    # Sicherstellen, dass das Matrix-Format korrekt ist
     if len(matrix) != 15 or any(len(row) != 9 for row in matrix):
-        # print(f"Debug: Matrix hat Größe {len(matrix)}x{len(matrix[0])}")  # Debug-Print
-        raise ValueError("Matrix muss 15x9 sein.")
+        raise ValueError("Matrix must be 15x9.")
 
-    # Ränder, um den abgeschnittenen Bereich des TVs zu berücksichtigen
-    border_x = screen_width * 0.09  # 5% des Bildschirms als Rand
+    border_x = screen_width * 0.09
     border_y = screen_height * 0.11
-
-    # Abstand zwischen den Zahlen berechnen
     spacing_x = (screen_width - 2 * border_x) / 14.3
     spacing_y = (screen_height - 2 * border_y) / 7.8
 
-    # Bildschirm mit Schwarz füllen
     screen.fill((0, 0, 0))
 
     for x, column in enumerate(matrix):
         for y, number in enumerate(column):
-            # Farbe basierend auf der y-Position festlegen und dann anpassen
             base_color = colors[y // 3]
             color = adjust_color(base_color)
-
-            # Wenn die Zahl 0 ist, setze die Farbe auf Weiß
             if number == 0:
                 color = zero_color
-
             text = font.render(str(number), True, color)
-            pos_x = border_x + x * spacing_x + random.randint(-5, 5)  # Abweichung in X-Richtung
-            pos_y = screen_height - border_y - y * spacing_y + random.randint(-5, 5)  # Abweichung in Y-Richtung
+            pos_x = border_x + x * spacing_x + random.randint(-5, 5)
+            pos_y = screen_height - border_y - y * spacing_y + random.randint(-5, 5)
             screen.blit(text, (pos_x, pos_y))
-
     pygame.display.flip()
 
 
 @hdmi_in_thread
 def hdmi_intro_animation():
+    """Play a screen animation simulating a matrix bootup."""
     global screen, font, colors, zero_color
 
-    n = 4  # Die Frequenz
-    current_cycle = 0  # Zählt die Zyklen
-
-    # Eine Liste, die die letzten wackelnden Positionen speichert
+    n = 4 # frequency
+    current_cycle = 0
     last_randoms = [[(0, 0) for _ in range(9)] for _ in range(15)]
-
-    # Ränder für die abgeschnittenen Bereiche des Bildschirms
     border_x = screen_width * 0.09
     border_y = screen_height * 0.11
-
-    # Gruppen von Reihen, die nacheinander animiert werden (Reihenfolge umgedreht)
     groups = [6, 7, 8], [3, 4, 5], [0, 1, 2]
 
+    # Function to draw a matrix column with shake effects
     def draw_column(col, rows):
         for row in rows:
-            text = font.render('0', True, zero_color)  # Weiß für die Ziffer 0
+            text = font.render('0', True, zero_color)
             spacing_x = (screen_width - 2 * border_x) / 14.3
             spacing_y = (screen_height - 2 * border_y) / 7.8
 
-            # Wenn der aktuelle Zyklus durch n teilbar ist, berechne eine neue Wackelposition
             if current_cycle % n == 0:
                 random_x = random.randint(-5, 5)
                 random_y = random.randint(-5, 5)
@@ -147,48 +132,41 @@ def hdmi_intro_animation():
 
     for idx, group in enumerate(groups):
         for column in range(15):
-            # Bildschirm mit Schwarz füllen
             screen.fill((0, 0, 0))
 
-            # Zeichne bereits gezeichnete Gruppen
             for prev_group in groups[:idx]:
                 for prev_column in range(15):
                     draw_column(prev_column, prev_group)
 
-            # Zeichne bereits gezeichnete Spalten für die aktuelle Gruppe
             for prev_column in range(column):
                 draw_column(prev_column, group)
 
-            # Zeichne die aktuelle Spalte für die aktuelle Gruppe
             draw_column(column, group)
 
             pygame.display.flip()
             pygame.time.wait(int(60 / n))
 
-            # Aktualisiere den Zykluszähler
             current_cycle += 1
 
 
 @hdmi_in_thread
 def hdmi_outro_animation():
+    """Play a screen animation simulating a matrix shutdown."""
     global screen, font, colors, zero_color
 
-    n = 4  # Die Frequenz
-    current_cycle = 0  # Zählt die Zyklen
+    n = 4  # frequency
+    current_cycle = 0
 
-    # Eine Liste, die die letzten wackelnden Positionen speichert
     last_positions = [[(0, 0) for _ in range(9)] for _ in range(15)]
 
-    # Ränder für die abgeschnittenen Bereiche des Bildschirms
     border_x = screen_width * 0.09
     border_y = screen_height * 0.11
 
-    # Gruppen von Reihen, die nacheinander animiert werden (von unten nach oben)
     groups = [0, 1, 2], [3, 4, 5], [6, 7, 8]
 
+    # Function to draw a matrix column with shake effects
     def draw_column(col, rows):
         for row in rows:
-            # Wenn der aktuelle Zyklus durch n teilbar ist, berechne eine neue Wackelposition
             if current_cycle % n == 0:
                 spacing_x = (screen_width - 2 * border_x) / 14.3
                 spacing_y = (screen_height - 2 * border_y) / 7.8
@@ -198,41 +176,21 @@ def hdmi_outro_animation():
             else:
                 pos_x, pos_y = last_positions[col][row]
 
-            text = font.render('0', True, zero_color)  # Weiß für die Ziffer 0
+            text = font.render('0', True, zero_color)
             screen.blit(text, (pos_x, pos_y))
 
     for idx, group in enumerate(groups):
         for column in reversed(range(15)):
-            # Bildschirm mit Schwarz füllen
             screen.fill((0, 0, 0))
 
-            # Zeichne Gruppen, die noch nicht verschwunden sind
-            for visible_group in groups[idx+1:]:
+            for visible_group in groups[idx + 1:]:
                 for visible_column in range(15):
                     draw_column(visible_column, visible_group)
 
-            # Zeichne Spalten für die aktuelle Gruppe, die noch nicht verschwunden sind
             for visible_column in range(column):
                 draw_column(visible_column, group)
 
             pygame.display.flip()
             pygame.time.wait(int(100 / n))
 
-            # Aktualisiere den Zykluszähler
             current_cycle += 1
-
-
-"""
-# Testmatrix zum Zeichnen
-matrix = [
-    [1, 2, 3, 4, 5, 6, 7, 8, 9] for _ in range(15)
-]
-
-
-while True:
-    draw_matrix(matrix)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-"""
