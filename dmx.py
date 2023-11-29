ae0fa82dca5701aba44e4b91811d6dc4ba2dae2a
admiral-vulcan <82753542+admiral-vulcan@users.noreply.github.com>
2023-11-29 18:02:28 +0100

    performance upgrade

--- dmx.py
+++ dmx.py
@@ -12,10 +12,9 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+import threading
 import requests
-import time
 from helpers import *
-
 
 # Erstelle eine HTTP-Session
 session = requests.Session()
@@ -24,9 +23,23 @@ session = requests.Session()
 dmx_values = ['0'] * 512
 
 
-def set_dmx_value(address, value):
-    # Aktualisiere den entsprechenden Wert in der Liste
-    dmx_values[address - 1] = str(value)
-
-    # Sende die gesamte DMX-Liste als einen String
+def send_dmx_values():
     session.post('http://localhost:9090/set_dmx', data={'u': '0', 'd': ','.join(dmx_values)})
+
+
+def set_dmx_value(address, value):
+    # Prüfe, ob der Wert sich geändert hat
+    if dmx_values[address - 1] != str(value):
+        dmx_values[address - 1] = str(value)
+        threading.Thread(target=send_dmx_values).start()
+
+
+def set_dmx_values_from(address, values):
+    changed = False
+    for i, value in enumerate(values):
+        if address + i < len(dmx_values) and dmx_values[address + i - 1] != str(value):
+            dmx_values[address + i - 1] = str(value)
+            changed = True
+
+    if changed:
+        threading.Thread(target=send_dmx_values).start()
